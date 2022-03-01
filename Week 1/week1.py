4d23733d16ae578e9550a66c30204a1174004852
Fikri Putra Hidayat <93188486+ApolloFikz13@users.noreply.github.com>
2022-03-01 15:03:37 +0700

    Update week1.py

--- Week 1/week1.py
+++ Week 1/week1.py
@@ -1,2 +1,4 @@
 #Week 1 Machine Learning
 print("Fikri Putra Hidayat \n 1103190100")
+print("Intro to Machine Learning on Kaggle")
+print("link to my collab : https://colab.research.google.com/drive/1nUm-zXriXjcPTvYLyrWc07s1zYDopKkv?usp=sharing ")
